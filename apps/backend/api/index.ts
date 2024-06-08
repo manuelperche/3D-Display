f688bd2d890565a0eedf7a08d3b5b55a92dbd414
manuelperche <manuelperche@gmail.com>
2024-06-08 00:37:47 -0400

    remove logger

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,4 +1,4 @@
-import { log } from "@repo/logger";
+// import { log } from "@repo/logger";
 import { createServer } from "./server";
 
 const port = process.env.PORT || 5001;
@@ -6,7 +6,7 @@ const server = createServer();
 
 server.listen(port, () => {
   // eslint-disable-next-line -- logger
-  log(`api running on ${port}`);
+  // log(`api running on ${port}`);
 });
 
 module.exports = server;
