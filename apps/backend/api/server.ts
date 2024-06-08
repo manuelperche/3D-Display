2ad3ada389a93bfdd9a3f4fee93f9004e792fc79
manuelperche <manuelperche@gmail.com>
2024-06-08 14:02:52 -0400

    test on the server

--- apps/backend/api/server.ts
+++ apps/backend/api/server.ts
@@ -8,12 +8,17 @@ import { appRouter } from "./routers";
 export const createServer = (): Express => {
   const app = express();
 
+  const corsOptions = {
+    origin: 'https://3-d-display-frontend.vercel.app',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
   app
     .disable("x-powered-by")
     .use(morgan("dev"))
     .use(urlencoded({ extended: true }))
     .use(json())
-    .use(cors());
+    .use(cors(corsOptions));
 
   app.use("/trpc", createExpressMiddleware({ router: appRouter }));
 
